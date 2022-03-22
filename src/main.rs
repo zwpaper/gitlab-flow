6dc1887ec1327f707e8582143900d8f0525dc634
WeiZhang <kweizh@gmail.com>
2022-03-22 22:33:24 +0800

    :sparkles: use gitlab and wecom dep

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
+use std::env;
+
 use actix_web::{web, App, HttpServer};
 
 mod gitlab;
-mod wecom;
 
 use crate::gitlab::webhook::webhook;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let key: &'static str = "hello";
+    let key = match env::var("GF_WECOM_WEBHOOK_KEY") {
+        Ok(val) => val,
+        Err(e) => panic!("{}", e),
+    };
     HttpServer::new(move || {
         App::new().service(
             web::resource("/webhook")
-                .app_data(web::Data::new(key))
+                .app_data(web::Data::new(key.clone()))
                 .route(web::post().to(webhook)),
         )
     })
