fdeaf059e57a412e9eb53399424f146059ecf951
WeiZhang <kweizh@gmail.com>
2022-03-28 00:19:50 +0800

    :hammer: notify: send md instead of text

--- src/gitlab/webhook.rs
+++ src/gitlab/webhook.rs
@@ -1,14 +1,36 @@
-use actix_web::{web, Error, HttpResponse, Responder};
-use gitlab::webhooks;
+use actix_web::{web, Error, HttpResponse};
+use gitlab::{types, webhooks};
 use wecom::webhook as wc;
 
 // #[post("/webhook")]
 pub async fn webhook(
     body: web::Json<webhooks::WebHook>,
-    key: web::Data<&str>,
+    key: web::Data<String>,
 ) -> Result<HttpResponse, Error> {
     println!("{:?}", body);
-    let result = wc::new(key.get_ref()).notify_text("hello world!").await;
+    let msg = match body.0 {
+        webhooks::WebHook::MergeRequest(mr) => {
+            let mra = mr.object_attributes;
+            let action = match mra.action.unwrap_or(webhooks::MergeRequestAction::Open) {
+                webhooks::MergeRequestAction::Open => "🥚 MR New",
+                webhooks::MergeRequestAction::Update => "🐣 MR Update",
+                webhooks::MergeRequestAction::Close  => "🦆 MR Close",
+                webhooks::MergeRequestAction::Reopen => "🐥 MR Reopen",
+                webhooks::MergeRequestAction::Approved => "🐤 MR Approved",
+                webhooks::MergeRequestAction::Unapproved => "🐦 MR Unapproved",
+                webhooks::MergeRequestAction::Merge => "🐔 MR Merge",
+            };
+            format!(
+                "# {}: [{}]({})\n- assigned: <@{}>",
+                action,
+                mra.title,
+                mra.url.unwrap(),
+                mra.assignee_id.unwrap_or(types::UserId::new(0)).value(),
+            )
+        }
+        _ => format!(":checkered_flag:"),
+    };
+    let result = wc::new(key.get_ref()).notify_markdown(&msg).await;
     println!("result: {:?}", result);
     match result {
         Ok(_) => Ok(HttpResponse::Ok().body("ok")),
